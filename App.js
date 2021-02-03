f821443efc92a31609e0731f8ec7b5565c85f6cf
Sanlim <5910110581@psu.ac.th>
2021-02-03 17:05:32 +0700

    add preview

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import PreviewScreen from './screens/PreviewScreen';
 
 const App = () => {
   return(
-    <View style={styles.container}>
-      <Text>
-        Sanlim Nisoy
-      </Text>
-    </View>
+    <PreviewScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
 
 export default App;
